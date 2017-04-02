6be2e0d9b7e45e8c95cddd39b32e9520e83f74fb
Michael Gallaspy <gallaspy.michael@gmail.com>
2017-04-02 11:32:33 -0700

    Move to get_images script

---
+++ get_images.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+import re
+import requests
+
+from os.path import join
+from pprint import pprint
+from googleapiclient.discovery import build
+
+# Project imports
+import secrets
+
+def get_images(term, dest, num=40):
+    total = 0
+    nonalnum_expr = re.compile(r"[^\w\d]+")
+    try:
+        os.makedirs(img_dir)
+    except os.error:
+        pass
+
+    service = build("customsearch", "v1", developerKey=secrets.apikey)
+
+    while total < num:
+        res = service.cse().list(
+          q=term,
+          cx=secrets.searchid,
+          searchType='image',
+          num=10,
+          start=total,
+        ).execute()
+
+        total += len(res['items'])
+        
+        for item in res['items']:
+            resp = requests.get(item['link'])
+            if resp.status_code == requests.codes.ok:
+                cleaned_title = nonalnum_expr.sub("_", item['title']) + '.jpg'
+                filename = join(img_dir, cleaned_title)
+                with open(filename, 'wb') as f:
+                    f.write(resp.content)
+
+if __name__ == "__main__":
+    get_images('lemon tree', 'unsorted_images')
+    for term in ['motorcycles', 'faces', 'owls', 'city']:
+        get_images(term, 'neg_images')
